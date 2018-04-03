823f21fbefbe6a71e6571e2c86bbf9c5256dc191
Phuong <phuongtinhbien12@gmail.com>
2018-04-04 05:13:43 +0700

    admin 4/4/2018 - Them anh - search fix xong -AsyncHTTPAPI

--- app/src/main/java/com/example/vuphu/app/AcsynHttp/AsyncHttpApi.java
+++ app/src/main/java/com/example/vuphu/app/AcsynHttp/AsyncHttpApi.java
@@ -1,9 +1,14 @@
 package com.example.vuphu.app.AcsynHttp;
 
+import android.content.Context;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.HttpEntity;
 
 public class AsyncHttpApi {
     private static final String BASE_URL = NetworkConst.network;
@@ -17,8 +22,8 @@ public class AsyncHttpApi {
     public static void post(String url, RequestParams params, JsonHttpResponseHandler responseHandler) {
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
-    public static void put(String url, RequestParams params, JsonHttpResponseHandler responseHandler) {
-        client.put(getAbsoluteUrl(url), params, responseHandler);
+    public static void put(Context context, String url, RequestParams params, JsonHttpResponseHandler responseHandler) {
+        client.put(context,url,params, responseHandler);
     }
 
 
