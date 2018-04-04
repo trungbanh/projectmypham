58d34c548e4e4682338874ff94e2adde4a3f5a5c
Phuong <phuongtinhbien12@gmail.com>
2018-04-04 18:06:24 +0700

    admin 4/4/2018 chỉnh lại order làm trang order admin

--- app/src/main/java/com/example/vuphu/app/admin/adapter/AdminOrdersAdapter.java
+++ app/src/main/java/com/example/vuphu/app/admin/adapter/AdminOrdersAdapter.java
@@ -1,9 +1,14 @@
 package com.example.vuphu.app.admin.adapter;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.content.res.AppCompatResources;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +22,7 @@ import com.example.vuphu.app.R;
 import com.example.vuphu.app.admin.AdminEditProductActivity;
 import com.example.vuphu.app.object.order;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import org.json.JSONObject;
 
@@ -30,18 +36,20 @@ import cz.msebera.android.httpclient.Header;
 
 public class AdminOrdersAdapter {
 
+
+
     private static class orderViewHolder extends RecyclerView.ViewHolder{
 
 
-        private TextView tv_name, tv_price,tv_date,tv_status;
-        private ImageButton btn_edit, btn_delete;
+        private TextView tv_name, tv_product,tv_quantity,tv_status;
+        private ImageButton btn_paid, btn_delete;
         public orderViewHolder(View itemView) {
             super(itemView);
             tv_name = itemView.findViewById(R.id.tv_admin_order_name);
-            tv_date = itemView.findViewById(R.id.tv_admin_order_date);
-            tv_price = itemView.findViewById(R.id.tv_admin_order_price);
+            tv_quantity = itemView.findViewById(R.id.tv_admin_order_quantity);
+            tv_product = itemView.findViewById(R.id.tv_admin_order_product);
             tv_status  = itemView.findViewById(R.id.tv_admin_order_status);
-            btn_edit = itemView.findViewById(R.id.btn_admin_order_edit);
+            btn_paid = itemView.findViewById(R.id.btn_admin_order_check_paid);
             btn_delete = itemView.findViewById(R.id.btn_admin_order_delete);
 
         }
@@ -52,10 +60,14 @@ public class AdminOrdersAdapter {
         ArrayList<order> list;
         Context context;
         String token;
+        private ProgressDialog progressBar;
         public orderAdap(ArrayList<order> list, Context context, String token) {
             this.list = list;
             this.context = context;
             this.token = token;
+            progressBar = new ProgressDialog(context);
+            progressBar.setMessage("Đang xử lí...");
+
         }
 
         @Override
@@ -64,17 +76,28 @@ public class AdminOrdersAdapter {
             return new orderViewHolder(v);
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         @Override
         public void onBindViewHolder(orderViewHolder holder, final int position) {
 
 
             holder.tv_name.setText(list.get(position).getOwnerUid());
-            holder.tv_price.setText(list.get(position).getProduct());
-            holder.tv_date.setText(list.get(position).getQuatityBuy());
+            holder.tv_product.setText(list.get(position).getProduct());
+            holder.tv_quantity.setText(list.get(position).getQuatityBuy());
             holder.tv_status.setText(list.get(position).getStatus());
+
+            if (list.get(position).getStatus().equals("paid")){
+                ColorStateList csl = AppCompatResources.getColorStateList(context, R.color.colorPrimary);
+                holder.btn_paid.setImageTintList(csl);
+            }
+            else{
+                ColorStateList csl = AppCompatResources.getColorStateList(context, android.R.color.black);
+                holder.btn_paid.setImageTintList(csl);
+            }
             holder.btn_delete.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+
                     new AlertDialog.Builder(context)
                             .setTitle("Xóa")
                             .setMessage("Bạn có chắc muốn xóa?")
@@ -84,24 +107,37 @@ public class AdminOrdersAdapter {
                                 public void onClick(DialogInterface dialog, int whichButton) {
                                     //Lap trinh o day
                                     //product/delete/<idproduct>
+                                    progressBar.show();
                                     AsyncHttpApi.delete(token,"/orders/"+list.get(position).get_id(),new JsonHttpResponseHandler(){
                                         @Override
                                         public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                                             super.onSuccess(statusCode, headers, response);
                                             list.remove(position);
                                             AdminOrdersAdapter.orderAdap.super.notifyDataSetChanged();
+                                            progressBar.hide();
                                         }
                                     });
                                 }})
                             .setNegativeButton(android.R.string.no, null).show();
                 }
             });
-            holder.btn_edit.setOnClickListener(new View.OnClickListener() {
+            holder.btn_paid.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(context, AdminEditProductActivity.class);
-                    intent.putExtra("data", list.get(position));
-                    context.startActivity(intent);
+
+                    list.get(position).setStatus("paid");
+                    RequestParams params = new RequestParams();
+                    params.put("status",list.get(position).getStatus());
+                    AsyncHttpApi.put(token,"/orders/"+list.get(position).get_id(),params
+                            ,new JsonHttpResponseHandler(){
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                            super.onSuccess(statusCode, headers, response);
+                            progressBar.hide();
+                            Toast.makeText(context, response.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    AdminOrdersAdapter.orderAdap.super.notifyDataSetChanged();
                 }
             });
         }
